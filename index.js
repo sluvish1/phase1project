8c033dfc30c10d35c25d2ac22009d52e7d6fcb6e
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-11 19:26:06 -0400

    1st commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-/*DOM*/
-let updoform = document.getElementById(`upDoform`)
-console.log(updoform.querySelectorAll(`div`))
+    /*when the booking button is clicked a form will pop up for the user to
+    pick a day and time of the appointment once appt is set user will then => click the
+    next button so they may pick a payment option(in person cash or cashapp payment) 
+    dendending on which box the user clicks "in person" payments will ask user how much cash will they be bring so that
+    i will have their change will have a box for ppl who will bring exact amount, will have a blank for couponds codes will 
+    once thats done user must click the 'book my appt button` that will have a pop 
+    up of an overveiw of the booking details, once the user comfimers everything user must click submit 
+    */
+
+    //adding click events to book button
+document.getElementById("upDoBookingButton").addEventListener
+(`click`, () => alert(`hello govna`))
+
+document.getElementById("braidedPonytailBookingButton").addEventListener
+(`click`, () => alert(`hello govna`))
+
+
+document.getElementById("heartBunBookingButton").addEventListener
+(`click`, () => alert(`hello govna`))
+
+
+document.getElementById("pigtailsBookingButton").addEventListener
+(`click`, () => alert(`hello govna`))
+
+
