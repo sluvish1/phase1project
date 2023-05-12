fbbf9579173552af6db3c255cf2fa91514051480
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-12 11:59:40 -0400

    4 commit

--- index.js
+++ index.js
@@ -7,5 +7,51 @@ once thats done user must click the 'book my appt button` that will have a pop
 up of an overveiw of the booking details, once the user comfimers everything user must click submit 
 */
 
-
+//creates&adds the Event-Listener
+addEventListener(`DOMContentLoaded`,()=>{
     
+   document.getElementById(`about-btn`).addEventListener(`click`,(e)=>{
+    alert(`i was clicked`)
+    //info popup
+   });
+
+   document.getElementById(`review-btn`).addEventListener(`click`,(e)=>{
+    alert(`i was clicked`)
+    //review subbmitter pops up
+   });
+
+    document.getElementById(`login-btn`).addEventListener(`click`,(e)=>{
+       
+        const memberForm = document.createElement(`form`)
+        const memberInput = document.createElement(`input`)
+       
+     
+        memberForm.append(memberInput)
+        e.target.memberForm.value
+
+       
+       console.log(memberForm)
+    //signup and logins
+ });
+
+//adding events listenters to hairstyle scetion
+console.log(document.getElementById(`updo-form`))
+
+})
+
+
+
+
+
+//annoymous function passes in the navbtn objs and does a for-each loop
+
+
+ /* (navbtn)=> navbtn.forEach(btn => {
+    addEventListener(`click`,(btn)=>{
+        alert(b`hi`)
+    })
+  });
+*/
+    
+
+
