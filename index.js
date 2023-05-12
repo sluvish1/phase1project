69c8bfecb159bc75c161a8a996df917768c959fc
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-11 23:10:26 -0400

    2nd commit

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-    /*when the booking button is clicked a form will pop up for the user to
-    pick a day and time of the appointment once appt is set user will then => click the
-    next button so they may pick a payment option(in person cash or cashapp payment) 
-    dendending on which box the user clicks "in person" payments will ask user how much cash will they be bring so that
-    i will have their change will have a box for ppl who will bring exact amount, will have a blank for couponds codes will 
-    once thats done user must click the 'book my appt button` that will have a pop 
-    up of an overveiw of the booking details, once the user comfimers everything user must click submit 
-    */
+/*when the booking button is clicked a form will pop up for the user to
+pick a day and time of the appointment once appt is set user will then => click the
+next button so they may pick a payment option(in person cash or cashapp payment) 
+dendending on which box the user clicks "in person" payments will ask user how much cash will they be bring so that
+i will have their change will have a box for ppl who will bring exact amount, will have a blank for couponds codes will 
+once thats done user must click the 'book my appt button` that will have a pop 
+up of an overveiw of the booking details, once the user comfimers everything user must click submit 
+*/
 
-    //adding click events to book button
-document.getElementById("upDoBookingButton").addEventListener
-(`click`, () => alert(`hello govna`))
-
-document.getElementById("braidedPonytailBookingButton").addEventListener
-(`click`, () => alert(`hello govna`))
-
-
-document.getElementById("heartBunBookingButton").addEventListener
-(`click`, () => alert(`hello govna`))
-
-
-document.getElementById("pigtailsBookingButton").addEventListener
-(`click`, () => alert(`hello govna`))
-
-
+//adding click events to book button
+document.addEventListener(`DOMContentLoaded`, () => {
+    let allForms = document.querySelectorAll(`form`).forEach((singleForm) => {
+        return singleForm.addEventListener(`submit`, (e) => {
+            
+        })
+    })
+})
