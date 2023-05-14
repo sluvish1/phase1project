857828f058fa2a9ab51f5254eab7e7953906531a
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-13 22:41:31 -0400

    6th

--- index.js
+++ index.js
@@ -7,67 +7,80 @@ once thats done user must click the 'book my appt button` that will have a pop
 up of an overveiw of the booking details, once the user comfimers everything user must click submit 
 */
 
-//creates&adds the Event-Listener
+//Adds Event-Listener to the DOC
 addEventListener(`DOMContentLoaded`, () => {
-
+/*
     document.getElementById(`about-btn`).addEventListener(`click`, (e) => {
-
         alert(`i was clicked`)
-        //info popup
     });
 
     document.getElementById(`review-btn`).addEventListener(`click`, (e) => {
         alert(`i was clicked`)
         //review subbmitter pops up
     });
+    */
 
-    //when button is clicked opensMemberForm function will run
-    const membersBtn = document.getElementById(`members-btn`).addEventListener(`click`, (e) => {
 
-        // e.preventDefault()
-        //e.target.value
-        opensMemberSignup()
-    });
-    const modal = document.getElementById("modal");
-    const modalContent = document.querySelector(".modal-content")
-    const closeModalButton = document.getElementsByClassName("close")[0];
-    closeModalButton.addEventListener("click", function () {
-        modal.style.display = "none";
-    });
-    //Member Form Content
-    function opensMemberSignup() {
-        modal.style.display = "block";
-        const submitBtn = document.createElement(`input`)
-        submitBtn.type = "submit"
-        submitBtn.value = "create account"
-       
-        const memberForm = document.createElement(`form`)
-        const emailInput = document.createElement(`input`)
-        emailInput.style.display = "block"
-        emailInput.type = ("text")
-        emailInput.style.marginBottom = "10px"
-        emailInput.placeholder = "email"
-        const passwordInput = document.createElement(`input`)
-        passwordInput.type = ('text')
-        passwordInput.style.display = "block"
-        passwordInput.placeholder = "password   "
-        memberForm.append(emailInput, passwordInput, submitBtn)
-        modalContent.append(memberForm)
-        //console.log(memberForm)
-        // let formm;
-        //const memberForm = document.createElement(`form`).id = `member-form`
-        //const memberInfoSignIn = document.createElement(`input-type`).append(memberInfoSignIn);
-        memberForm.addEventListener("submit",(e)=>{
-            e.preventDefault()
-            console.log(`clicked`)
-        })
-
-        //memberInfoSignIn.innerText = document.querySelector(`member-form`
-    }
-
+    /* STEP 1: Will add a EvevntListenter thatll listen for the  button to be clicked , 
+    once clicked itll run the opensMemberForm function*/
+    const coupondAlertMsg = document.createElement(`div`);
+    const childEle = document.createElement(`p`).id=`hello`
+    const parentEle = document.createElement(`h2`)
+   coupondAlertMsg =  coupondAlertMsg.append(childEle,parentEle)
    
+   
+
+    const coupondsBtn = document.getElementById(`coupond-btn`).addEventListener(`click`, (e) => {
+       alert()
+    });
+    
+    
 })
 
+
+    /* const modal = document.getElementById("modal");
+     const modalContent = document.querySelector(".modal-content")
+     const closeModalButton = document.getElementsByClassName("close")[0];
+     closeModalButton.addEventListener("click", function () {
+         modal.style.display = "none";
+     });
+ 
+     // STEP : Member Form Content
+     function opensMemberSignup() {
+        / modal.style.display = "block";
+         const submitBtn = document.createElement(`input`)
+         submitBtn.type = "submit"
+         submitBtn.value = "Create Account"
+ 
+         const memberForm = document.createElement(`form`)
+         const emailInput = document.createElement(`input`)
+         emailInput.style.display = "block"
+         emailInput.type = ("text")
+         emailInput.style.marginBottom = "10px"
+         emailInput.placeholder = "email"
+         const passwordInput = document.createElement(`input`)
+         passwordInput.type = ('text')
+         passwordInput.style.display = "block"
+         passwordInput.placeholder = "password"
+         memberForm.append(emailInput, passwordInput, submitBtn)
+         modalContent.append(memberForm)
+ 
+ 
+  
+ 
+         memberForm.addEventListener("submit", (e) => {
+             e.preventDefault()
+             console.log(`clicked`)
+         })
+         //END OF OLD CODE
+         
+ 
+         //memberInfoSignIn.innerText = document.querySelector(`member-form`
+     }
+ 
+
+}
+ */
 
 
 
@@ -81,6 +94,3 @@ addEventListener(`DOMContentLoaded`, () => {
    })
  });
 */
-
-
-
